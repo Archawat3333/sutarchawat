7f2b225f205c820790e7fe053137c73931e5a477
Archawat3333 <121007264+Archawat3333@users.noreply.github.com>
2023-01-22 22:58:10 +0700

    Create main.go

---
+++ backend/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"github.com/B6310851/team05/controller"
+	"github.com/B6310851/team05/entity"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CORSMiddleware() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+
+			c.AbortWithStatus(204)
+
+			return
+
+		}
+
+		c.Next()
+
+	}
+
+}
+
+func main() {
+
+	entity.SetupDatabase()
+
+	r := gin.Default()
+
+	r.Use(CORSMiddleware())
+
+	// User Routes
+
+	r.GET("/courses", controller.ListCourses)
+	r.GET("/course/:id", controller.GetCourse)
+	r.POST("/courses", controller.CreateCourse)
+
+	r.GET("/qualifications", controller.ListQualifications)
+	r.GET("/qualification/:qualification_id", controller.GetQualification)
+	r.POST("qualifications", controller.CreateQualification)
+
+	r.GET("/majors", controller.ListMajors)
+	r.GET("/major/:major_id", controller.GetMajor)
+	r.POST("majors", controller.CreateMajor)
+
+	// Run the server
+
+	r.Run()
+
+}
